5505eeb3d7f70df5c4fc519e5e755beaba026960
alvaroth <425164@student.saxion.nl>
2021-12-16 16:23:37 +0100

    changed field order for product and shop added labels with translations changed price to list type so it can hold multiple price versions

--- site/templates/shop.php
+++ site/templates/shop.php
@@ -6,9 +6,9 @@
 
 <div class="row">
     <div class="col-md-2 ml-4 ">
+        <h4>Bedrijfsniveau</h4>
         <div class="list-group pr-5">
-            <h4>Product Tags</h4>
-            <?php foreach ($site->find('tags')->tags_product()->split() as $tag): ?>
+            <?php foreach ($site->find('tags')->tags_company()->split() as $tag): ?>
                 <label class="list-group-item">
                     <input class="form-check-input me-1" type="checkbox"
                            value="<?= preg_replace("/[^a-z0-9]+/i", "", $tag); ?>"
@@ -17,9 +17,9 @@
                 </label>
             <?php endforeach ?>
         </div>
-        <h4>Company Tags</h4>
+        <h4>Productniveau</h4>
         <div class="list-group pr-5">
-            <?php foreach ($site->find('tags')->tags_company()->split() as $tag): ?>
+            <?php foreach ($site->find('tags')->tags_product()->split() as $tag): ?>
                 <label class="list-group-item">
                     <input class="form-check-input me-1" type="checkbox"
                            value="<?= preg_replace("/[^a-z0-9]+/i", "", $tag); ?>"
@@ -32,20 +32,20 @@
     <div class="col-md-8 ml-4 ">
         <div class="row">
             <?php foreach ($page->children()->listed() as $product): ?>
-            <div class="col-md-2 border mr-3 ml-3 mb-3">
-                <div class="product <?= getClassSafeUrl($product) ?>" id="<?= $product ?>">
-                    <a href="<?= $product->link() ?>">
-                        <img class="img-fluid img-thumbnail" src="<?= $product->image()->url() ?>" width="200vh"
-                             height="auto">
-                        <figcaption class="text">
-                            <h3 class="product-title"><?= $product->title() ?></h3>
-                            <h6 class="product-short-description"><?= $product->short_description() ?></h6>
-                            <h6 class="product-product-tags badge badge-secondary"><?= $product->product_tags() ?></h6>
-                            <h6 class="product-company-tags badge badge-secondary"><?= $product->company_tags() ?></h6>
-                        </figcaption>
-                    </a>
+                <div class="col-md-2 border mr-3 ml-3 mb-3">
+                    <div class="product <?= getClassSafeUrl($product) ?>" id="<?= $product ?>">
+                        <a href="<?= $product->link() ?>">
+                            <img class="img-fluid img-thumbnail" src="<?= $product->image()->url() ?>" width="200vh"
+                                 height="auto">
+                            <figcaption class="text">
+                                <h3 class="product-title"><?= $product->title() ?></h3>
+                                <h6 class="product-short-description"><?= $product->short_description() ?></h6>
+                                <h6 class="product-product-tags badge badge-secondary"><?= $product->product_tags() ?></h6>
+                                <h6 class="product-company-tags badge badge-secondary"><?= $product->company_tags() ?></h6>
+                            </figcaption>
+                        </a>
+                    </div>
                 </div>
-            </div>
             <?php endforeach ?>
         </div>
     </div>
@@ -79,7 +79,7 @@ function getClassSafeUrl($product)
                 noneChecked = false;
                 $(".product." + js_tag).parent().show();
             }
-        if(noneChecked) {
+        if (noneChecked) {
             $(".product").parent().show();
         }
     }
